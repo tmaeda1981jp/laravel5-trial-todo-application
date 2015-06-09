b0f5f37fa73f322d4d26936cc4438b8334a13cfa
tmaeda1981jp <tmaeda1981jp@gmail.com>
2015-06-10 03:25:51 +0900

    Fix typo Tasks->Task

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,14 +13,13 @@
 Route::model('tasks', 'Task');
 Route::model('projects', 'Project');
 
-Route::resource('projects', 'ProjectsController');
-//Route::resource('tasks', 'TasksController');
-Route::resource('projects.tasks', 'TasksController');
-
 Route::bind('tasks', function ($value, $route) {
-    return App\Tasks::whereSlug($value)->first();
+    return App\Task::whereSlug($value)->first();
 });
 
 Route::bind('projects', function ($value, $route) {
     return App\Project::whereSlug($value)->first();
 });
+
+Route::resource('projects', 'ProjectsController');
+Route::resource('projects.tasks', 'TasksController');
